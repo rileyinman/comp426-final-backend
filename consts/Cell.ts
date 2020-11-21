e099945902db5a2319061cd0edff755f19d0b20c
Jasper Christie <jasp@live.unc.edu>
2020-11-20 21:38:21 -0500

    added keys and doors

--- consts/Cell.ts
+++ consts/Cell.ts
@@ -1,7 +1,17 @@
 enum Cell {
   PLAYER = 'player',
   WALL = 'wall',
-  KEY = 'key'
+  KEY1 = 'key1',
+  KEY2 = 'key2',
+  KEY3 = 'key3',
+  KEY4 = 'key4',
+  KEY5 = 'key5',
+  FLOOR = 'floor',
+  DOOR1 = 'door1',
+  DOOR2 = 'door2',
+  DOOR3 = 'door3',
+  DOOR4 = 'door4',
+  DOOR5 = 'door5'
 }
 
 export default Cell;
