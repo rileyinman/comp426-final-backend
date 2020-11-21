4db7e089a7bdb0464497fbef426b13f4f71424b9
s-fin <shyleaf@live.unc.edu>
2020-11-20 23:00:03 -0500

    minor adjustments

--- levels/Level0.ts
+++ levels/Level0.ts
@@ -1,6 +1,7 @@
 import { Cell } from '../consts/index.js';
 import Level from '../Level.js';
 
+
 let Level0 =
   new Level(0, 10, 10, [
     Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.DOOR5, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL,
