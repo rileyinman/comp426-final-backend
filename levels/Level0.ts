be0cfc362a9f389962530573e25cbf97bccd2a84
s-fin <shyleaf@live.unc.edu>
2020-11-20 21:55:18 -0500

    changed size of level0 board

--- levels/Level0.ts
+++ levels/Level0.ts
@@ -2,7 +2,7 @@ import { Cell } from '../consts/index.js';
 import Level from '../Level.js';
 
 let Level0 =
-  new Level(0, 5, 5, [
+  new Level(0, 10, 10, [
     Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL,
     Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL,
     Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL,
