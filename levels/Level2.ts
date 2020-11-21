9f4a2b3dcd295476d0ca53be7bf497ac8ff4b4af
s-fin <shyleaf@live.unc.edu>
2020-11-20 23:18:19 -0500

    added level2 board and added level2 to levels index

---
+++ levels/Level2.ts
@@ -0,0 +1,19 @@
+import { Cell } from '../consts/index.js';
+import Level from '../Level.js';
+
+
+let Level2 =
+  new Level(2, 10, 10, [
+    Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.DOOR5, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL,
+    Cell.WALL, Cell.KEY2, Cell.WALL, Cell.KEY2, Cell.WALL, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.NPC, Cell.WALL,
+    Cell.WALL, Cell.FLOOR, Cell.DOOR3, Cell.FLOOR, Cell.WALL, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.WALL,
+    Cell.WALL, Cell.FLOOR, Cell.WALL, Cell.FLOOR, Cell.WALL, Cell.FLOOR, Cell.FLOOR, Cell.WALL, Cell.WALL, Cell.WALL,
+    Cell.WALL, Cell.FLOOR, Cell.WALL, Cell.KEY4, Cell.WALL, Cell.FLOOR, Cell.FLOOR, Cell.DOOR1, Cell.KEY5, Cell.WALL,
+    Cell.WALL, Cell.FLOOR, Cell.WALL, Cell.WALL, Cell.WALL, Cell.DOOR4, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL,
+    Cell.WALL, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.WALL,
+    Cell.WALL, Cell.DOOR2, Cell.WALL, Cell.DOOR2, Cell.WALL, Cell.DOOR1, Cell.WALL, Cell.WALL, Cell.DOOR4, Cell.WALL,
+    Cell.WALL, Cell.KEY4, Cell.WALL, Cell.KEY3, Cell.WALL, Cell.FLOOR, Cell.KEY1, Cell.WALL, Cell.KEY3, Cell.WALL,
+    Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.PLAYER, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL
+  ]);
+
+export default Level2;
--- levels/index.ts
+++ levels/index.ts
@@ -1,9 +1,10 @@
 import { LevelData } from '../consts/index.js';
 import Level0 from './Level0.js'
 import Level1 from './Level1.js';
+import Level2 from './Level2.js';
 
 const Levels = [
-  Level0, Level1
+  Level0, Level1, Level2
 ];
 
 for (const level of Levels) {
