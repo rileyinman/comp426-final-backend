219f3d08a6e664445fed30166c4e59c0a184b94d
Jasper Christie <jasp@live.unc.edu>
2020-11-20 22:30:00 -0500

    added level1 board layout

--- levels/Level1.ts
+++ levels/Level1.ts
@@ -3,11 +3,16 @@ import Level from '../Level.js';
 
 let Level1=
   new Level(1, 10, 10, [
-    Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL,
-    Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, 
-    Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL,
-    Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL,
-    Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL
+    Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.DOOR5, Cell.WALL, Cell.WALL,
+    Cell.WALL, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.WALL, Cell.WALL,
+    Cell.WALL, Cell.WALL, Cell.WALL, Cell.DOOR2, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL,
+    Cell.WALL, Cell.KEY5, Cell.WALL, Cell.FLOOR, Cell.FLOOR, Cell.KEY2, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.WALL,
+    Cell.WALL, Cell.FLOOR, Cell.WALL, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.FLOOR, Cell.WALL,
+    Cell.WALL, Cell.DOOR4, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.DOOR3, Cell.WALL,
+    Cell.WALL, Cell.FLOOR, Cell.FLOOR, Cell.DOOR2, Cell.FLOOR, Cell.FLOOR, Cell.KEY2, Cell.FLOOR, Cell.FLOOR, Cell.WALL,
+    Cell.WALL, Cell.FLOOR, Cell.FLOOR, Cell.WALL, Cell.WALL, Cell.DOOR1, Cell.WALL, Cell.WALL, Cell.DOOR2, Cell.WALL,
+    Cell.WALL, Cell.FLOOR, Cell.FLOOR, Cell.WALL, Cell.KEY1, Cell.FLOOR, Cell.WALL, Cell.WALL, Cell.KEY3, Cell.WALL,
+    Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.PLAYER, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL, Cell.WALL
   ]);
 
 export default Level1;
