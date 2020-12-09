83e0db5d80e23b9340183f2bff88e708bb1c23b3
Riley Inman <rileyminman@gmail.com>
2020-12-08 19:21:36 -0500

    Fix player registration

--- consts/Player.ts
+++ consts/Player.ts
@@ -1,6 +1,8 @@
 enum Player {
-  PLAYER1 = "player1",
-  PLAYER2 = "player2"
+  PLAYER1 = 'player1',
+  PLAYER2 = 'player2',
+  PLAYER3 = 'player3',
+  PLAYER4 = 'player4'
 }
 
 export default Player;
